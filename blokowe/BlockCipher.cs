b445cf02b71bcbe84e6ac1be2560076e03a398ed
BartoszKosmider <bartekkosmider123@gmail.com>
2021-11-19 23:09:43 +0100

    aaa

---
+++ blokowe/BlockCipher.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
+using System.Text;
+
+public class BlockCipher 
+{
+	public BlockCipher(CipherMode mode)
+	{
+		this.mode = mode;
+	}
+
+	public string EncryptString(string key, string message)
+	{
+		var byteKey = Encoding.UTF8.GetBytes(key);
+		var byteMessage = Encoding.UTF8.GetBytes(message);
+		byte[] array;
+
+		var ecbBlock = new AesManaged()
+		{
+			//KeySize = 128,
+			Key = byteKey,
+			//BlockSize = 128,
+			Mode = mode,
+			//Padding = PaddingMode.Zeros,
+			IV = new byte[16]
+		};
+
+		var encryptor = ecbBlock.CreateEncryptor(ecbBlock.Key, ecbBlock.IV);
+		using var memoryStream = new MemoryStream();
+		using var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+		{
+			using (var streamWriter = new StreamWriter(cryptoStream))
+			{
+				streamWriter.Write(message);
+			}
+
+			array = memoryStream.ToArray();
+		}
+
+		return Convert.ToBase64String(array); ;
+	}
+
+	public string DecryptString(string key, string message)
+	{
+		var byteKey = Encoding.UTF8.GetBytes(key);
+		var byteMessage = Convert.FromBase64String(message);
+
+		var ecbBlock = new AesManaged()
+		{
+			//KeySize = 128,
+			Key = byteKey,
+			//BlockSize = 128,
+			Mode = mode,
+			//Padding = PaddingMode.Zeros,
+			IV = new byte[16]
+		};
+
+		var decryptor = ecbBlock.CreateDecryptor(ecbBlock.Key, ecbBlock.IV);
+		using var memoryStream = new MemoryStream(byteMessage);
+		using var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+		using var streamReader = new StreamReader(cryptoStream);
+		return streamReader.ReadToEnd();
+	}
+
+	private CipherMode mode;
+}
---
+++ blokowe/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace blokowe
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Console.WriteLine("1 - ecb, 2 - cbc, 3 - cfb");
+				var inputValue = Console.ReadLine();
+				switch (inputValue)
+				{
+					case "1":
+					{
+						Ecb();
+						break;
+					}
+					case "2":
+					{
+						Cbc();
+						break;
+					}
+					case "3":
+					{
+						Cfb();
+						break;
+					}
+					default:
+					{
+						Console.WriteLine("invalid data provided");
+						break;
+					}
+				}
+			}
+		}
+
+		private static void Ecb()
+		{
+			var ecb = new BlockCipher(CipherMode.ECB);
+			Console.WriteLine("input value to encrypt:");
+			var input = Console.ReadLine();
+			Console.WriteLine($"input value: {input}, {input.Length}");
+			var encryptedMessage = ecb.EncryptString(key, input);
+			Console.WriteLine($"encrypted message: {encryptedMessage}, {encryptedMessage.Length}");
+			var decryptedMessage = ecb.DecryptString(key, encryptedMessage);
+			Console.WriteLine($"decrypted message: {decryptedMessage}, {decryptedMessage.Length}");
+		}
+
+		private static void Cbc()
+		{
+			var cbc = new BlockCipher(CipherMode.CBC);
+			Console.WriteLine("input value to encrypt:");
+			var input = Console.ReadLine();
+			Console.WriteLine($"input value: {input}, {input.Length}");
+			var encryptedMessage = cbc.EncryptString(key, input);
+			Console.WriteLine($"encrypted message: {encryptedMessage}, {encryptedMessage.Length}");
+			var decryptedMessage = cbc.DecryptString(key, encryptedMessage);
+			Console.WriteLine($"decrypted message: {decryptedMessage}, {decryptedMessage.Length}");
+		}
+
+		private static void Cfb()
+		{
+			var cfb = new BlockCipher(CipherMode.CFB);
+			Console.WriteLine("input value to encrypt:");
+			var input = Console.ReadLine();
+			Console.WriteLine($"input value: {input}, {input.Length}");
+			var encryptedMessage = cfb.EncryptString(key, input);
+			Console.WriteLine($"encrypted message: {encryptedMessage}, {encryptedMessage.Length}");
+			var decryptedMessage = cfb.DecryptString(key, encryptedMessage);
+			Console.WriteLine($"decrypted message: {decryptedMessage}, {decryptedMessage.Length}");
+		}
+
+		private static string key = "0000000000000000";
+	}
+}
